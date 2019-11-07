3455c6f4fc729784e431564f059ff27c4ed59ec6
myname21-meet <myname21@meet.mit.edu>
2019-11-07 11:44:33 -0500

    7th nov

--- app.py
+++ app.py
@@ -6,10 +6,25 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 
 
 ##### Code here ######
-@app.route("/home")
+@app.route("/")
 def home():
 	return render_template("home.html")
 
+
+
+@app.route("/store")
+def store():
+	return render_template("store.html")
+
+
+@app.route("/cart")
+def cart():
+	return render_template("cart.html")
+
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
 #####################
 
